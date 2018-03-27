62eb75ba8d6abc17af3b6846c3fab3398033e886
lkrsnik <krsnik.luka92@gmail.com>
2018-03-27 11:41:37 +0200

    Prepared bidirectional basic input

--- cnn/word_accetuation/cnn_dictionary/v5_1/workbench.py
+++ cnn/word_accetuation/cnn_dictionary/v5_1/workbench.py
@@ -27,10 +27,12 @@ from prepare_data import *
 # save_inputs('../../internal_representations/inputs/shuffeled_matrix_validate_inputs_other_features_output_11.h5', X_validate, y_validate,  other_features = X_other_features_validate)
 # X_train, X_other_features_train, y_train = load_inputs('cnn/internal_representations/inputs/shuffeled_matrix_train_inputs_other_features_output_11.h5', other_features=True)
 # X_validate, X_other_features_validate, y_validate = load_inputs('cnn/internal_representations/inputs/shuffeled_matrix_validate_inputs_other_features_output_11.h5', other_features=True)
-data = Data('l', bidirectional_basic_input=True)
+data = Data('l', bidirectional_basic_input=True, bidirectional_architectural_input=True)
 data.generate_data('letters_word_accetuation_bidirectional_train',
                    'letters_word_accetuation_bidirectional_test',
-                   'letters_word_accetuation_bidirectional_validate')
+                   'letters_word_accetuation_bidirectional_validate', content_name='SlovarIJS_BESEDE_utf8.lex',
+                      content_shuffle_vector='content_shuffle_vector', shuffle_vector='shuffle_vector',
+                      inputs_location='cnn/internal_representations/inputs/', content_location='data/')
 
 
 num_examples = len(data.x_train)  # training set size
@@ -45,7 +47,7 @@ num_fake_epoch = 20
 
 
 
-conv_input_shape=(46, 36)
+conv_input_shape=(23, 36)
 othr_input = (140, )
 
 conv_input = Input(shape=conv_input_shape, name='conv_input')
@@ -54,9 +56,16 @@ x_conv = Conv1D(46, (3), padding='same', activation='relu')(x_conv)
 x_conv = MaxPooling1D(pool_size=2)(x_conv)
 x_conv = Flatten()(x_conv)
 
+conv_input2 = Input(shape=conv_input_shape, name='conv_input2')
+x_conv2 = Conv1D(115, (3), padding='same', activation='relu')(conv_input2)
+x_conv2 = Conv1D(46, (3), padding='same', activation='relu')(x_conv2)
+x_conv2 = MaxPooling1D(pool_size=2)(x_conv2)
+x_conv2 = Flatten()(x_conv2)
+# x_conv = Dense(516, activation='relu', kernel_constraint=maxnorm(3))(x_conv)
+
 othr_input = Input(shape=othr_input, name='othr_input')
 
-x = concatenate([x_conv, othr_input])
+x = concatenate([x_conv, x_conv2, othr_input])
 # x = Dense(1024, input_dim=(516 + 256), activation='relu')(x)
 x = Dense(256, activation='relu')(x)
 x = Dropout(0.3)(x)
@@ -69,16 +78,16 @@ x = Dense(nn_output_dim, activation='sigmoid')(x)
 
 
 
-model = Model(inputs=[conv_input, othr_input], outputs=x)
+model = Model(inputs=[conv_input, conv_input2, othr_input], outputs=x)
 opt = optimizers.Adam(lr=1E-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=[actual_accuracy,])
 # model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 
-history = model.fit_generator(data.generator('train', batch_size),
+history = model.fit_generator(data.generator('train', batch_size, content_name='SlovarIJS_BESEDE_utf8.lex', content_location='data/'),
                               data.x_train.shape[0]/(batch_size * num_fake_epoch),
                               epochs=actual_epoch*num_fake_epoch,
-                              validation_data=data.generator('test', batch_size),
+                              validation_data=data.generator('test', batch_size, content_name='SlovarIJS_BESEDE_utf8.lex', content_location='data/'),
                               validation_steps=data.x_test.shape[0]/(batch_size * num_fake_epoch),
                               verbose=2
                               )
