c5edd96075bd4c8d7812407dda27787ce176d3ad
Luka <krsnik.luka92@gmail.com>
2018-04-05 10:25:08 +0200

    Expanded SVM inputs - added other features

--- cnn/word_accetuation/svm/svm.py
+++ cnn/word_accetuation/svm/svm.py
@@ -32,19 +32,42 @@ data.generate_data('../../internal_representations/inputs/letters_word_accentuat
                    '../../internal_representations/inputs/letters_word_accentuation_test',
                    '../../internal_representations/inputs/letters_word_accentuation_validate')
 
-reshaped_train = np.reshape(data.x_train, (data.x_train.shape[0], 828))
-reshaped_test = np.reshape(data.x_test, (data.x_test.shape[0], 828))
+reshaped_train = np.reshape(data.x_train, (data.x_train.shape[0], data.x_train.shape[1] * data.x_train.shape[2]))
+concatenated_train = np.concatenate((reshaped_train, data.x_other_features_train), axis=1)
+reshaped_test = np.reshape(data.x_test, (data.x_test.shape[0], data.x_test.shape[1] * data.x_test.shape[2]))
+concatenated_test = np.concatenate((reshaped_test, data.x_other_features_test), axis=1)
 
-for i in range(-5, 5):
-    c = math.pow(10, i)
 
-    svm = LinearSVC(random_state=0, verbose=True, max_iter=10000, C=c, class_weight='balanced')
+
+c_search = False
+
+
+if c_search:
+    for i in range(-5, 5):
+        c = math.pow(10, i)
+
+        svm = LinearSVC(random_state=0, verbose=True, max_iter=10000, C=c, class_weight='balanced')
+        cls = BinaryRelevance(classifier=svm)
+
+        cls.fit(reshaped_train, data.y_train)
+
+        filename = 'finalized_model_c_balanced_' + str(c) + '.sav'
+        pickle.dump(cls, open(filename, 'wb'))
+
+        result = cls.score(reshaped_test, data.y_test)
+        print('c = ' + str(c) + ' || result = \n' + str(result))
+
+else:
+    c = 1.0
+
+    svm = LinearSVC(random_state=0, verbose=True, max_iter=10000, C=c)
     cls = BinaryRelevance(classifier=svm)
 
     cls.fit(reshaped_train, data.y_train)
 
-    filename = 'finalized_model_c_balanced_' + str(c) + '.sav'
+    filename = 'complete_input.sav'
     pickle.dump(cls, open(filename, 'wb'))
 
     result = cls.score(reshaped_test, data.y_test)
-    print('c = ' + str(c) + ' || result = ' + str(result))
+    print('c = ' + str(c) + ' || result = \n' + str(result))
+
