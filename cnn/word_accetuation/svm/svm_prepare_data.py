03ce9e5b3e4946d51916873fdceb624b35bcf299
Luka <krsnik.luka92@gmail.com>
2018-04-11 13:47:54 +0200

    Added accent type classification tests

--- cnn/word_accetuation/svm/svm_prepare_data.py
+++ cnn/word_accetuation/svm/svm_prepare_data.py
@@ -22,20 +22,20 @@ data = Data(mode)
 if mode == 'l':
     data.generate_data('../../internal_representations/inputs/letters_word_accentuation_train',
                        '../../internal_representations/inputs/letters_word_accentuation_test',
-                       '../../internal_representations/inputs/letters_word_accentuation_validate')
+                       '../../internal_representations/inputs/letters_word_accentuation_validate', test_set=True)
 else:
     data.generate_data('../../internal_representations/inputs/syllables_word_accetuation_train',
                        '../../internal_representations/inputs/syllables_word_accetuation_test',
-                       '../../internal_representations/inputs/syllables_word_accetuation_validate')
+                       '../../internal_representations/inputs/syllables_word_accetuation_validate', test_set=True)
 
 final_train = sparse.csr_matrix(np.concatenate((np.reshape(data.x_train, (data.x_train.shape[0], 828)), data.x_other_features_train), axis=1))
 final_test = sparse.csr_matrix(np.concatenate((np.reshape(data.x_test, (data.x_test.shape[0], 828)), data.x_other_features_test), axis=1))
 
-filename = 'data/letters_word_accentuation_x_train.pkl'
+filename = 'data/letters_word_accentuation_x_test_train.pkl'
 pickle.dump(final_train, open(filename, 'wb'))
-filename = 'data/letters_word_accentuation_x_test.pkl'
+filename = 'data/letters_word_accentuation_x_test_test.pkl'
 pickle.dump(final_test, open(filename, 'wb'))
-filename = 'data/letters_word_accentuation_y_train.pkl'
+filename = 'data/letters_word_accentuation_y_test_train.pkl'
 pickle.dump(data.y_train, open(filename, 'wb'))
-filename = 'data/letters_word_accentuation_y_test.pkl'
+filename = 'data/letters_word_accentuation_y_test_test.pkl'
 pickle.dump(data.y_test, open(filename, 'wb'))
--- run_multiple_files.py
+++ run_multiple_files.py
@@ -4,3 +4,8 @@ import cnn.word_accetuation.cnn_dictionary.v5_2.workbench
 import cnn.word_accetuation.syllables.v3_2.workbench
 # import cnn.word_accetuation.syllabled_letters.v3_0.workbench
 import cnn.word_accetuation.syllabled_letters.v3_2.workbench
+
+#cnn/accent_classification/syllabled_letters/v2_0/
+import cnn.accent_classification.letters.v3_0.workbench
+import cnn.accent_classification.syllables.v2_0.workbench
+import cnn.accent_classification.syllabled_letters.v2_0.workbench
